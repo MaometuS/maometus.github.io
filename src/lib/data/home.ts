30795c5dde15266cced6400f4f4cc725afc0ac06
Mukhammad Rakhimov <maometusu@gmail.com>
2024-05-26 20:18:28 +0900

    fixed a typo in the home page

--- src/lib/data/home.ts
+++ src/lib/data/home.ts
@@ -8,7 +8,7 @@ export const name = 'Mukhammad';
 export const lastName = 'Rakhimov';
 
 export const description =
-	'I have been fascinated with coding all my conscious life. I build my first program at the age of 10 and never looked back since. I have been exploring the world of coding both professionally and as a leisure. I am always excited solve complex problems by building versatile systems. One of my proudest projects is the platform Reqlama.uz, which is a large advertising aggregation system. Looking forward to helping more clients with their various automation issues. If you are interested in working with me, please reach out. I will be glad to talk to you.';
+	'I have been fascinated with coding all my conscious life. I built my first program at the age of 10 and never looked back since. I have been exploring the world of coding both professionally and as a leisure. I am always excited solve complex problems by building versatile systems. One of my proudest projects is the platform Reqlama.uz, which is a large advertising aggregation system. Looking forward to helping more clients with their various automation issues. If you are interested in working with me, please reach out. I will be glad to talk to you.';
 
 export const links: Array<{ platform: Platform; link: string }> = [
 	{ platform: Platform.GitHub, link: 'https://github.com/MaometuS' },
